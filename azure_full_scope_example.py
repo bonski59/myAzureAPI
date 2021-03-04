a7aa3f221217c40ed08d7ad38ced58e8e2a3a040
bonski59 <60044219+bonski59@users.noreply.github.com>
2021-03-03 19:37:20 -0700

    Initial Commit
    n/a

---
+++ azure_full_scope_example.py
@@ -0,0 +1,127 @@
+import os, ntpath
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, blockblobservice
+from configparser import ConfigParser
+
+config_file = r".\config.ini"
+config = ConfigParser()
+config.read(config_file)
+
+class azureDevTools:
+    
+    connect_str = config['azure']['connect_str']
+    container_name = config['azure']['container']
+
+    # Create the BlobServiceClient object which will be used to create a container client
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+    # get existing container
+    try:
+        container_client = blob_service_client.get_container_client(container_name)
+    except Exception as ex:
+        print('Exception: container client issue')
+        print(ex)
+
+    @classmethod
+    def dev_mk_txt_file(cls, filename):  # add file to dev-testing container on Azure
+        try:
+            filename = filename + ".txt"
+            # Create a local directory to hold blob data
+            uploads_path = "{}/upload".format(os.getcwd())
+            if not os.path.exists(uploads_path):
+                os.mkdir(uploads_path)
+
+            # Create a file in the local data directory to upload and download
+
+            upload_file_path = os.path.join(uploads_path, filename)
+
+            # Write text to the file
+            file = open(upload_file_path, 'w')
+            file.write("Hello, World!")
+            file.close()
+
+            # Create a blob client using the local file name as the name for the blob
+
+            blob_client = cls.blob_service_client.get_blob_client(container=cls.container_name, blob=filename)
+
+            print("\nUploading to Azure Storage as blob:\n\t" + filename)
+
+            # Upload the created file
+            with open(upload_file_path, "rb") as data:
+                blob_client.upload_blob(data)
+
+            os.remove(upload_file_path)
+
+        except Exception as xx:
+            print('Exception: dev_mk_txt_file()')
+            print(xx)
+
+        return
+
+    @classmethod
+    def upload_file(cls, filepath):
+        # confirm fp exist
+        # if exist send
+        head, tail = ntpath.split(filepath)
+        if not os.path.exists(filepath):
+            print("filepath does not exist: {}".format(tail))
+            return
+        else:
+            print("\n Uploading File: {}".format(tail))
+            try:
+                blob_client = cls.blob_service_client.get_blob_client(container=cls.container_name, blob=tail)
+                blob_client.upload_blob(filepath)
+                print("\t Upload Successful: {}".format(tail))
+            except Exception as xx:
+                print('Upload Aborted for the Following')
+                print('\t Exception: upload_file()')
+                print("\t " + str(xx))
+
+        return
+
+    @classmethod
+    def list_blobs(cls):
+        print("\nListing blobs...")
+        blob_arr = []
+        # List the blobs in the container
+        blob_list = cls.container_client.list_blobs()
+        print("Container: {}".format(cls.container_name))
+        for blob in blob_list:
+            blob_arr.append(blob.name)
+            print("\t" + blob.name)
+        return blob_arr
+
+    @classmethod
+    def delete_container(cls):
+            cls.container_client.delete_container()
+
+    @classmethod
+    def delete_blob(cls, blob_names):
+        if not type(blob_names) == list:
+            blob_names = [blob_names]
+        for i in blob_names:
+            cls.container_client.delete_blobs(i)
+
+    @classmethod
+    def dev_download(cls, cloud_filename):
+        if not type(cloud_filename) == list:
+            cloud_filename = [cloud_filename]
+        downloads_path = r"{}\download".format(os.getcwd())
+
+        print("\nDownloading blob to \n\t" + downloads_path)
+        try:
+            for i in cloud_filename:
+                download_file_path = r"{}\{}".format(downloads_path, i)
+                blob_client = cls.blob_service_client.get_blob_client(container=cls.container_name, blob=i)
+                with open(download_file_path, "wb") as download_file:
+                    download_file.write(blob_client.download_blob().readall())
+                print("\t Download Successful: {}".format(i))
+
+        except Exception as xx:
+            print('Download Aborted for the Following')
+            print('\t Exception: dev_download()')
+            print("\t " + str(xx))
+
+
+
+# testing
+# azureDevTools.dev_mk_txt_file("potato3")
